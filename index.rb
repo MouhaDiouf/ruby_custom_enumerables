1139c806b5e64860d3ebc7d65103b8d840d8424d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2021-11-15 11:16:37 +0000

    create methods my_each my_each_with_index & select

---
+++ index.rb
@@ -0,0 +1,120 @@
+module Enumerable
+    def my_each
+        if block_given?
+            for i in 0...self.length 
+                yield(self[i])
+            end 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+    def my_each_with_index
+        if block_given?
+            for i in 0...self.length 
+                yield(self[i], i)
+            end 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+    def my_select 
+        result = []
+        if block_given?
+            for i in 0...self.length 
+                if yield(self[i]) == true
+                    result << self[i]
+                end 
+            end
+            result 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+    def my_all?
+        if block_given?
+            for i in 0...self.length 
+                if yield(self[i]) == false
+                    return false 
+                end 
+            end
+            true 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+    def my_any?
+        if block_given?
+            for i in 0...self.length 
+                if yield(self[i]) == true
+                    return true 
+                end 
+            end
+            false 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+
+    def my_none?
+        if block_given?
+            for i in 0...self.length 
+                if yield(self[i]) == true
+                    return false 
+                end 
+            end
+            true 
+        else 
+            puts "You must provide a block"
+        end
+    end 
+
+    def my_count(arg = nil)
+        count = 0
+
+        if arg 
+            for i in 0...self.length 
+                if arg == self[i]
+                    count += 1
+                end 
+            end 
+            count 
+        elsif block_given?
+            for i in 0...self.length 
+                if yield(self[i]) == true
+                    count += 1
+                end 
+            end
+            count 
+        else 
+            self.length
+        end
+    end 
+
+end 
+
+puts "my_each vs. each"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each  { |item| puts item }
+numbers.each  { |item| puts item }
+
+
+puts "my_each_with_index vs. each_with_index"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each_with_index  { |item, idx| puts "item, #{idx}" }
+numbers.each_with_index { |item, idx| puts "item, #{idx}" }
+puts "my all"
+p numbers.my_all? {|item| item > 1}
+puts "my any"
+p numbers.my_any? {|item| item > 1}
+puts "my none"
+p numbers.my_none? {|item| item > 0}
+puts "my count"
+ary = [1, 2, 4, 2]
+p ary.my_count                  #=> 4
+p ary.my_count(2)               #=> 2
+p ary.my_count { |x| x%2 == 0 } #=> 3
